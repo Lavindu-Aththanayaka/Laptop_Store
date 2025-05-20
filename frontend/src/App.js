2d6810051fa84dc646f6e0230f8db77ecb9773eb
Lavindu-Aththanayaka <gihanaththanayakavj@gmail.com>
2025-05-21 00:41:20 +0530

    back end is connected to the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,13 @@ import "./App.css";
 import { Outlet } from "react-router-dom";
 import Header from "./component/Header";
 import Footer from "./component/Footer";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <>
+      <ToastContainer position="top-right" />
       <Header />
       <main className="min-h-[calc(100vh-56px)] pt-16">
         <Outlet />

5163bd0702ad44308e1fc7df6b7bd621ce3e2f2a
Lavindu-Aththanayaka <gihanaththanayakavj@gmail.com>
2025-05-20 17:57:03 +0530

    signup page is done

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -1,7 +1,175 @@
-import React from "react";
+import React, { useState } from "react";
+import LoginIcon from "../New folder/lock_9164513.png";
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
+import imageTobase64 from "../helpers/imageTobase64";
+
+import { toast } from "react-toastify";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setshowConfirmPassword] = useState(false);
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+    name: "",
+    confirmPassword: "",
+    profilePic: "",
+  });
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setData((preve) => {
+      return {
+        ...preve,
+        [name]: value,
+      };
+    });
+  };
+  const handleUploadPic = async (e) => {
+    const file = e.target.files[0];
+
+    const imagePic = await imageTobase64(file);
+
+    setData((preve) => {
+      return {
+        ...preve,
+        profilePic: imagePic,
+      };
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  console.log("data", data);
+  return (
+    <section id="signup">
+      <div className="mx-auto container p-4">
+        <div className="bg-custom-dark-or p-5 w-full max-w-sm mx-auto">
+          <div className="w-20 h-20 mx-auto relative overflow-hidden rounded-full">
+            <div>
+              <img src={data.profilePic || LoginIcon} alt="login icons" />
+            </div>
+            <form>
+              <label>
+                <div className="text-xs bg-opacity-80 bg-custom-dark-ora pb-4 pt-2 cursor-pointer text-center absolute bottom-0 w-full">
+                  Upload Photo
+                </div>
+                <input
+                  type="file"
+                  className="hidden"
+                  onChange={handleUploadPic}
+                />
+              </label>
+            </form>
+          </div>
+
+          <form className="pt-6 flex flex-col gap-2" onSubmit={handleSubmit}>
+            <div className="grid">
+              <label className="text-custom-dark-orange">Name : </label>
+              <div className="bg-custom-dark-ora p-2">
+                <input
+                  type="text"
+                  placeholder="enter your name"
+                  name="name"
+                  value={data.name}
+                  onChange={handleOnChange}
+                  required
+                  className="w-full h-full outline-none bg-transparent"
+                />
+              </div>
+            </div>
+            <div className="grid">
+              <label className="text-custom-dark-orange">Email : </label>
+              <div className="bg-custom-dark-ora p-2">
+                <input
+                  type="email"
+                  placeholder="enter email"
+                  name="email"
+                  value={data.email}
+                  onChange={handleOnChange}
+                  required
+                  className="w-full h-full outline-none bg-transparent"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label className="text-custom-dark-orange">Password : </label>
+              <div className="bg-custom-dark-ora p-2 flex">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  placeholder="enter password"
+                  value={data.password}
+                  name="password"
+                  onChange={handleOnChange}
+                  required
+                  className="w-full h-full outline-none bg-transparent"
+                />
+                <div
+                  className="cursor-pointer text-xl"
+                  onClick={() => setShowPassword((preve) => !preve)}
+                >
+                  <span>
+                    {showPassword ? (
+                      <FaEyeSlash className="text-custom-dark-or" />
+                    ) : (
+                      <FaEye className="text-custom-dark-or" />
+                    )}
+                  </span>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <label className="text-custom-dark-orange">
+                Confirm Password :
+              </label>
+              <div className="bg-custom-dark-ora p-2 flex">
+                <input
+                  type={showConfirmPassword ? "text" : "password"}
+                  placeholder="enter confirm password"
+                  required
+                  name="confirmPassword"
+                  value={data.confirmPassword}
+                  onChange={handleOnChange}
+                  className="w-full h-full outline-none bg-transparent"
+                />
+
+                <div
+                  className="cursor-pointer text-xl"
+                  onClick={() => setshowConfirmPassword((preve) => !preve)}
+                >
+                  <span>
+                    {showConfirmPassword ? (
+                      <FaEyeSlash className="text-custom-dark-or" />
+                    ) : (
+                      <FaEye className="text-custom-dark-or" />
+                    )}
+                  </span>
+                </div>
+              </div>
+            </div>
+
+            <button className="bg-custom-dark-orange hover:bg-orange-700 text-black px-6 py-2 w-full max-w-[150px] rounded-full hover:scale-110 transition-all mx-auto block mt-6">
+              Sign Up
+            </button>
+          </form>
+
+          <p className="my-5">
+            Already have account ?{" "}
+            <Link
+              to={"/login"}
+              className=" text-orange-500 hover:text-orange-700 hover:underline"
+            >
+              Login
+            </Link>
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default SignUp;
